8611b0c3d19aa5a421dc7ebc8b3eda285dbddc6a
Jon Pugh <jon@thinkdrop.net>
2018-04-19 15:49:27 -0400

    Include aegir/provision 3.x directly into Provision 4 as vendor code so we get all the core commands. Add a composer script to inject drushrc.php into drush so the provision commands are loaded without any external config.

---
+++ src/Provision/Console/ComposerScripts.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file ComposerScripts.php
+ *
+ * This file is not meant to be loaded by provision as it requires Composer classes.
+ *
+ * Currently it only contains one "script": writeDrushRcForVendorDrushDrush()
+ *
+ * This is used on composer install to copy a drushrc.php file into vendor/drush/drush.
+ *
+ * This drushrc.php file is used to include the provision 3.x drush commands.
+ *
+ */
+
+namespace Aegir\Provision\Console;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class ComposerScripts {
+
+    /**
+     * @param \Composer\Installer\PackageEvent $event
+     *
+     * @throws \Exception
+     */
+    public static function writeDrushRcForVendorDrushDrush(PackageEvent $event)
+    {
+        $root_dir = dirname(dirname(dirname(dirname(__FILE__)))) . '/vendor/drush/drush';
+        if ($event->getOperation()->getPackage()->getName() == 'drush/drush' && file_exists($root_dir) && is_writable($root_dir)) {
+            $drushrc = <<<PHP
+<?php
+\$options['include'] = array(
+    dirname(dirname(dirname(__FILE__))) . '/aegir/provision',
+);
+PHP;
+            if (file_put_contents($root_dir . '/drushrc.php', $drushrc)) {
+                print "Wrote drushrc.php to $root_dir \n";
+            }
+        }
+        else {
+            throw new \Exception("Directory $root_dir does not exist or is not writable. Provision cannot load it's commands.");
+        }
+    }
+}
